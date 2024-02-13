14ed21f36e6e2944c2fff119cab2d13b965dcc97
Adil <daraadil191@gmail.com>
2024-02-13 11:51:46 +0530

    Add Footer

---
+++ app.js
@@ -0,0 +1,92 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; // playerX , playerO
+let count = 0; //to track draw
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO === true) {
+      //playerO
+      box.innerText = "O";
+      box.style.color = "rgb(150,50,0)";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      box.style.color = "rgb(150,0,120)";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+    let isWinner = checkWinner();
+    if (count === 9 && !isWinner) {
+      checkDraw();
+    }
+  });
+});
+
+const checkDraw = () => {
+  msg.innerText = "Game was a draw!!!";
+  msg.style.color = "#330F0A";
+  msg.style.fontSize = "4rem";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations!!! The winner is ${winner}`;
+  msg.style.color = "#330F0A";
+  msg.style.fontSize = "4rem";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let indexVal1 = boxes[pattern[0]].innerText;
+    let indexVal2 = boxes[pattern[1]].innerText;
+    let indexVal3 = boxes[pattern[2]].innerText;
+
+    if (indexVal1 != "" && indexVal2 != "" && indexVal3 != "") {
+      if (indexVal1 === indexVal2 && indexVal2 === indexVal3) {
+        showWinner(indexVal1);
+        return true;
+      }
+    }
+  }
+};
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
